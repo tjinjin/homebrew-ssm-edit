04689b7d811b21c8e7fe36e1549dfab4e6b29128
tjinjin <tjinjinprogram@gmail.com>
2017-10-01 23:18:28 +0900

    Fix

--- ssm-edit.rb
+++ ssm-edit.rb
@@ -3,8 +3,7 @@ require 'formula'
 class SsmEdit < Formula
   homepage 'https://github.com/tjinjin/ssm-edit'
   version 'v0.0.1'
-
-  url 'https://github.com/tjinjin/ssm-edit/releases/download/v0.0.1/ssm-edit_v0.0.1_darwin_amd64.tar.gz'
+  url 'https://github.com/tjinjin/ssm-edit/releases/download/v0.0.1/ssm-edit_darwin_amd64.tar.gz'
   sha256 '5a25d128273175371509bef6b960060db8ecd32de2602b292357445840f1448f'
 
   def install
