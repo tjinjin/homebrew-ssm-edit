6a9261d44ed41d70bb652a308ff3ff25d457f967
tjinjin <tjinjinprogram@gmail.com>
2017-10-01 23:12:16 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## homebrew-ssm-edit
+
+Install ssm-edit via homebrew
---
+++ ssm-edit.rb
@@ -0,0 +1,13 @@
+require 'formula'
+
+class S3Edit < Formula
+  homepage 'https://github.com/tjinjin/ssm-edit'
+  version 'v0.0.1'
+
+  url 'https://github.com/tjinjin/ssm-edit/releases/download/v0.0.1/ssm-edit_v0.0.1_darwin_amd64.tar.gz'
+  sha256 '5a25d128273175371509bef6b960060db8ecd32de2602b292357445840f1448f'
+
+  def install
+    bin.install 'ssm-edit'
+  end
+end
